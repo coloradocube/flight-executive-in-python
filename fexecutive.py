02a373fa9c062ce533740f90f388dbb04080947b
Conlan Cason <ccason1@msudenver.edu>
2022-03-02 13:22:27 -0700

    Refactor to further improve readability, scalability, testability, and functionality

--- fexecutive.py
+++ fexecutive.py
@@ -1,96 +1,145 @@
+# TODO: add comments and documentation
+# TODO: create unit/integration tests
+# TODO: implement the code for the actual sensors
+# TODO: test on the actual sensors
+
+
 import time
 from datetime import datetime
 from collections import OrderedDict
+import csv
 
 
-# naming the headers to prevent typos
-timestamp, acc, tmp, env = ('timestamp', 'acc', 'tmp', 'env')
-
-# used to send the rockblock message only after a minimum amount of time has passed
-last_rockblock_sent_time = time.time()
-rockblock_interval_time = 4
-
-# used to only poll for data no sooner than when it makes sense to do so
-# to prevent unnecessary overhead
-start_times = {}
-
-# for testing purposes
-count = 0
-
-# a dictionary to compile the rockblock message
-names_list = [timestamp, acc, tmp, env]
-rockblock_dict = OrderedDict(zip(names_list, ['' for _ in names_list]))
+output_file_path = 'telemetry_logs'
 
 
-def log_to_csv(msg, filename):
-    pass
-        
+class TelemetryPoint:
 
-
-def update_rockblock_msg_and_log(name, min_time_interval, data_getter):
-        
-    if not name in start_times:
-        start_times[name] = time.time()
-        
-    # when enough time has passed, update the message and log
-    if time.time() - start_times[name] > min_time_interval:
-        
-        # reset the start time when a datum is logged
-        start_times[name] = time.time()
-        
-        # retrieve the data
-        data_to_log = data_getter()
-        
-        # update the dictionary with the most current data from this sensor
-        rockblock_dict[name] = data_to_log
-        
-        # append this data to its csv
-        #log_to_csv(msg, filename)
-        
-        
-def send_rockblock_msg():
+    global output_file_path
     
-    # to ensure that this static variable is used and updated globally
-    global last_rockblock_sent_time
+    def __init__(self, name, min_time_interval, data_getter):
     
-    rockblock_msg = ''
+        self.name = name
+        self.min_time_interval = min_time_interval
+        self.data_getter = data_getter
+        
+        self.start_time = time.time()
+        
+        filename = output_file_path + '/' + name + '_log.csv'
+        self.output_file = open(filename, 'a', newline='')
+        self.csv_writer = csv.writer(self.output_file)
+        
+        self.data = ''
+        
+        # write the header
+        self.csv_writer.writerow(['timestamp', self.name])
     
-    if time.time() - last_rockblock_sent_time > rockblock_interval_time:
-        rockblock_msg = ','.join([str(x) for x in rockblock_dict.values()])
-        last_rockblock_sent_time = time.time()
+    def is_ready_to_update(self):
+    
+        if time.time() - self.start_time > self.min_time_interval:
+            self.start_time = time.time()
+            return True
+        else:
+            return False
+    
+    def update(self):
+    
+            self.data = self.data_getter()
+    
+    def log_to_csv(self):
+    
+        self.csv_writer.writerow([get_timestamp(), self.data])
+
+
+class Rockblock:
+    
+    def __init__(self, min_time_interval=10):
+        
+        self.min_time_interval = min_time_interval
+    
+        self.start_time = time.time()
+    
+    def is_ready_to_send(self):
+    
+        if time.time() - self.start_time > self.min_time_interval:
+            self.start_time = time.time()
+            return True
+        else:
+            return False
+    
+    
+    def send(self, msg):
         
         # send rockblock message
-        print(rockblock_msg)
+        print(msg)
+
+
+def get_timestamp():
+    return datetime.now().isoformat(timespec='seconds')
+
+
+# TODO put this function into the TelemetryPoint class?
+def log(telemetry_point):
+    
+    # when enough time has passed, update the message and log
+    if telemetry_point.is_ready_to_update():
+        
+        # call the telemetry point's data getter function to retrieve the latest input data
+        telemetry_point.update()
+        
+        telemetry_point.log_to_csv()
 
 
 # flight loop
-while True:
+def main_execution_loop(telemetry_points, rockblock):
+    
+    for point in telemetry_points:
+        log(point)
+    
+    if rockblock.is_ready_to_send():
+        rockblock_msg = ','.join([p.data for p in telemetry_points])
+        rockblock.send(rockblock_msg)
+    
+    # For testing
+    time.sleep(0.1)
 
-    # reset the start time at the beginning of each iteration
-    iteration_start_time = time.time()
-    
-    rockblock_dict['timestamp'] = datetime.now().isoformat(timespec='seconds')
-    
+
+def setup_telemetry_points():
+
+    telemetry_points = []
+        
     def get_acc_data():
         #time.sleep(1)
-        return acc + str(count)
+        return 'acc'
     
-    update_rockblock_msg_and_log(name=acc, min_time_interval=5, data_getter=get_acc_data)
+    acc = TelemetryPoint(name='acc', min_time_interval=2, data_getter=get_acc_data)
+    telemetry_points.append(acc)
     
     def get_tmp_data():
         #time.sleep(1)
-        return tmp + str(count)
+        return 'tmp'
     
-    update_rockblock_msg_and_log(name=tmp, min_time_interval=5, data_getter=get_tmp_data)
+    tmp = TelemetryPoint(name='tmp', min_time_interval=5, data_getter=get_tmp_data)
+    telemetry_points.append(tmp)
     
     def get_env_data():
         #time.sleep(3)
-        return env + str(count)
+        return 'env'
     
-    update_rockblock_msg_and_log(name=env, min_time_interval=8, data_getter=get_env_data)
+    env = TelemetryPoint(name='env', min_time_interval=8, data_getter=get_env_data)
+    telemetry_points.append(env)
     
-    send_rockblock_msg()
+    return telemetry_points
+
+
+def main():
     
-    count += 1
+    telemetry_points = setup_telemetry_points()
+    rockblock = Rockblock(min_time_interval=4)
     
-    time.sleep(0.1)
+    while True:
+        main_execution_loop(telemetry_points, rockblock)
+
+
+if __name__ == '__main__':
+    main()
