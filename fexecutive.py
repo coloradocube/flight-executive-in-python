f0d686e71b04c5f3c07a6702a6e975d21680c738
Conlan Cason <ccason1@msudenver.edu>
2022-03-02 18:47:11 -0700

    Implement feature to monitor rockblock message count, which persists through reboot

--- fexecutive.py
+++ fexecutive.py
@@ -49,36 +49,85 @@ class TelemetryPoint:
     def log_to_csv(self):
     
         self.csv_writer.writerow([get_timestamp(), self.data])
+    
+    def close_file(self):
+        self.output_file.close()
 
 
 class Rockblock:
     
-    def __init__(self, min_time_interval=10):
+    def __init__(self, min_time_interval=10, max_messages=None, should_send=True):
         
         self.min_time_interval = min_time_interval
-    
+        self.max_messages = max_messages
+        self.should_send = should_send
+        
         self.start_time = time.time()
+        
+        msg_count_filename = 'rockblock_msg_count.txt'
+        self.msg_count_file = open(msg_count_filename, 'a+')
+        
+        self.msg_count = 0
+        
+        self.init_msg_count()
+    
+    def reset_msg_count(self):
+        self.msg_count = 0
+        self.msg_count_file.truncate(0)
+        self.msg_count_file.write(str(self.msg_count))
+    
+    def init_msg_count(self):
+        '''retrieves msg_count from a file in case of reboot'''
+        self.msg_count_file.seek(0)
+        
+        content = self.msg_count_file.read()
+        
+        if content == '':
+            self.msg_count_file.write(str(self.msg_count))
+        else:
+            self.msg_count = int(content)
+            
+    def increment_msg_count(self):
+        
+        self.msg_count += 1
+        self.msg_count_file.truncate(0)
+        self.msg_count_file.write(str(self.msg_count))
     
     def is_ready_to_send(self):
-    
-        if time.time() - self.start_time > self.min_time_interval:
+        
+        max_exceeded = False
+        
+        if self.max_messages == None:
+            max_exceeded = False
+        else:
+            max_exceeded = self.msg_count >= self.max_messages
+        
+        enough_time_passed = time.time() - self.start_time > self.min_time_interval
+        
+        if enough_time_passed and not max_exceeded:
             self.start_time = time.time()
             return True
         else:
             return False
     
-    
     def send(self, msg):
         
-        # send rockblock message
-        print(msg)
+        if self.should_send:
+            # send rockblock message
+            pass
+        else:
+            print(msg)
+        
+        self.increment_msg_count()
+    
+    def close_file(self):
+        self.msg_count_file.close()
 
 
 def get_timestamp():
     return datetime.now().isoformat(timespec='seconds')
 
 
-# TODO put this function into the TelemetryPoint class?
 def log(telemetry_point):
     
     # when enough time has passed, update the message and log
@@ -100,7 +149,7 @@ def main_execution_loop(telemetry_points, rockblock):
         rockblock_msg = ','.join([p.data for p in telemetry_points])
         rockblock.send(rockblock_msg)
     
-    # For testing
+    # for testing
     time.sleep(0.1)
 
 
@@ -135,10 +184,19 @@ def setup_telemetry_points():
 def main():
     
     telemetry_points = setup_telemetry_points()
-    rockblock = Rockblock(min_time_interval=4)
+    rockblock = Rockblock(min_time_interval=4, should_send=False)
+    
+    rockblock.reset_msg_count()
     
     while True:
+        # execute the flight loop
+        # set send_rockblock_data to False for testing
         main_execution_loop(telemetry_points, rockblock)
+    
+    for point in telemetry_points:
+        point.close_file()
+    
+    rockblock.close_file()
 
 
 if __name__ == '__main__':
