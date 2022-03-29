d410f085957a341b5764109e7de87df174a5cd23
Conlan Cason <ccason1@msudenver.edu>
2022-03-29 09:54:08 -0600

    Implement logging to csv

--- datasource.py
+++ datasource.py
@@ -1,25 +1,37 @@
 import time
+import csv
+import os
 
 
 class DataSource:
     
-    def __init__(self, frequency, getter_function, args=[], shutdown_q=None):
+    def __init__(self, file_path, header, frequency, getter_function, getter_args=[]):
         
         """Creates a new DataSource object
        
        parameters:
-       frequency - number of times the source should be polled per second
-       getter_function - the function that polls for the data
-       args - the arguments to the getter function (optional)
+       file_path: the output file path; of the form "file/path.csv"
+       header: a list of strings that will be the first line of the csv file, e.g. ['timestamp', 'lat', 'lon']
+       frequency: number of times the source should be polled per second
+       getter_function: the function that polls the sensors for data; must return a list
+       getter_args: a list of arguments to the getter function (optional)
        """
         
+        self.file_path = file_path
         self.frequency = frequency
         self.getter_function = getter_function
-        self.args = args
-        self.shutdown_q = shutdown_q
+        self.args = getter_args
+        self.header = header
         
         self.data = []
         self.shutdown = False
+        
+        self.output_file = open(self.file_path, 'a+', newline='')
+        self.csv_writer = csv.writer(self.output_file)
+        
+        if os.path.getsize(self.file_path) == 0:
+            self.csv_writer.writerow(header)
+        
     
     def poll(self):
         # Update the current data
@@ -27,9 +39,13 @@ class DataSource:
         self.data = self.getter_function(*self.args)
         return self.data
     
-    def log(self):
-        # to csv
-        print(self.frequency, 'logging')
+    def log(self, timestamp, data):
+        
+        # For demo purposes
+        print("log: {} {} {}".format(str(timestamp), str(self.frequency), str(data)))
+        
+        self.csv_writer.writerow([timestamp] + data)
+        
     
     def get_current_time_stamp():
         # from GNSS receiver or system
--- fexec.py
+++ fexec.py
@@ -1,45 +1,53 @@
 from datasource import DataSource
 import multiprocessing as mp
 import time
+from datetime import datetime
 from queue import Empty
 
+
+def get_timestamp():
+    return datetime.now().isoformat(timespec='seconds')
+
+
+def datasource_loop(ds, q, shutdown_q):
+    while shutdown_q.empty():
+        
+        current_data = ds.poll()
+        current_timestamp = get_timestamp()
+        
+        ds.log(current_timestamp, current_data)
+        
+        # Update the queue
+        if not q.empty():
+            q.get()
+        msg = {current_timestamp: current_data}
+        q.put(msg)
+        
+        time.sleep(1/ds.frequency)
 
 def f1(name, q, shutdown_q):
 
     def getter(name):
-        return name
+        return [name, 'says', 'hi']
     
-    ds = DataSource(2, getter, [name])
+    ds = DataSource('./f1_log.csv', ['timestamp', 'name', 'msg1', 'msg2'], 2, getter, [name])
     
-    while shutdown_q.empty():
-        ds.log()
-        print(ds.poll())
-        time.sleep(1/ds.frequency)
-    
-    # the data to return for the rockblock message
-    # TODO: put actual timestamp
-    msg = {'timestamp': ds.poll()}
-    q.put(msg)
+    datasource_loop(ds, q, shutdown_q)
+
 
 def f2(name, q, shutdown_q):
     
     def getter(name):
-        return name
+        return [name, 'says', 'hi']
     
-    ds = DataSource(3, getter, [name])
+    ds = DataSource('./f2_log.csv', ['timestamp', 'name', 'msg1', 'msg2'], 3, getter, [name])
     
-    while shutdown_q.empty():
-        ds.log()
-        print(ds.poll())
-        time.sleep(1/ds.frequency)
-    
-    msg = {'timestamp': ds.poll()}
-    q.put(msg)
+    datasource_loop(ds, q, shutdown_q)
 
 
 def create_process(data_qs, shutdown_q, ps, f, name):
     
-    # mp.Queue defaults: FIFO, infinite capacity
+    # Defaults for mp.Queue: FIFO, infinite capacity
     q = mp.Queue()
     data_qs.append(q)
     
@@ -50,36 +58,55 @@ def create_process(data_qs, shutdown_q, ps, f, name):
 
 def handle_processes():
     
+    # The queue to trigger a shutdown
     shutdown_q = mp.Queue()
+    
+    # Queues that receive regularly updated data from each datasource
     data_qs = []
+    
+    # Each datasource runs in its own process
     ps = []
     
     create_process(data_qs, shutdown_q, ps, f1, 'bob')
     create_process(data_qs, shutdown_q, ps, f2, 'caroline')
     
-    
-    time.sleep(2)
-    shutdown_q.put(True)
-    
-    for p in ps:
-        p.join()
+    time.sleep(1)
     
     for q in data_qs:
         try:
             print(q.get_nowait())
         except Empty:
-            print("Empty exception")
+            print("Empty exception: queue is empty")
+    
+    time.sleep(1)
+    
+    for q in data_qs:
+        try:
+            print(q.get_nowait())
+        except Empty:
+            print("Empty exception: queue is empty")
+    
+    time.sleep(1)
+    
+    shutdown_q.put(True)
+    
+    for q in data_qs:
+        try:
+            print(q.get_nowait())
+        except Empty:
+            print("Empty exception: queue is empty")
+    
+    for p in ps:
+        p.join()
     
     shutdown_q.get()
 
 
 if __name__ == '__main__':
     
-    # default for Linux
+    # Default for Linux
     mp.set_start_method('spawn')
     
     p_handler_p = mp.Process(target=handle_processes)
     p_handler_p.start()
-    
-    print("stuff")
         
