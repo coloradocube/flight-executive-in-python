46f28b827b905f704579b8801e29fd782b4d9e86
Conlan Cason <ccason1@msudenver.edu>
2022-05-09 09:12:30 -0600

    Added some documentation

--- fexec.py
+++ fexec.py
@@ -14,6 +14,12 @@ import packing
 
 
 def datasource_loop(ds, q, shutdown_q):
+    """Calls log on the datasource and loads the queue; shuts down if needed
+    
+    ds - A datasource object
+    q - The queue where the data will be loaded to be collected by the parent process
+    shutdown_q - Empty until ready to shutdown
+    """
     
     start_time = time.time()
     while shutdown_q.empty():
@@ -33,6 +39,7 @@ def datasource_loop(ds, q, shutdown_q):
             time.sleep(0.01)
         
     ds.close_file()
+
 
 def tmp117(q, shutdown_q, name):
     
@@ -54,6 +61,13 @@ def gnss(q, shutdown_q, name):
 
 
 def create_sensor_process(data_qs, shutdown_q, ps, f, name):
+    """Initializes each sensor's process along with its queue
+    
+    data_qs - The list of queues that will be used to transfer data from the datasource to the parent process
+    shutdown_q - The queue that is used to gracefully shutdown the datasource loops
+    ps - The list of processes to which this process will be added
+    f - A callback function that initializes the datasource and begins the datasource loop
+    """
     
     # Defaults for mp.Queue: FIFO, infinite capacity
     q = mp.Queue()
@@ -65,6 +79,7 @@ def create_sensor_process(data_qs, shutdown_q, ps, f, name):
 
 
 def init_rockblock():
+    """Returns an adafruit rockblock object"""
     
     # Get correct port
     port = None
@@ -79,6 +94,12 @@ def init_rockblock():
 
 
 def rockblock_send(data_q, ready_q, shutdown_q):
+    """Sends packed data from the RPi through the RockBLOCK; keep in mind this costs money and requires an open view of the clear sky
+    
+    data_q - A queue containing each packet of bytes ready to send through the RockBLOCK
+    ready_q - This function fills the ready_q when it is ready to send
+    shutdown_q - Allows this function to gracefully shutdown when the parent fills this queue
+    """
     
     rb = init_rockblock()
     print("rb model: " + rb.model)
@@ -103,6 +124,7 @@ def rockblock_send(data_q, ready_q, shutdown_q):
 
 
 def handle_processes():
+    """The parent of sensor and RockBLOCK processes; controls the flow of data between processes"""
     
     # The queue to trigger a shutdown
     shutdown_q = mp.Queue()
--- packing.py
+++ packing.py
@@ -11,7 +11,7 @@ tmp117_multiplier = 10**2
 tmp117_format = 'h'
 
 def pack_timestamp(ts):
-    
+    """Given a timestamp, returns the packed timestamp"""
     time = ts.split('T')[1]
     split_time = time.split(':')
     hour = int(split_time[0])
@@ -22,7 +22,7 @@ def pack_timestamp(ts):
 
 
 def format_unpacked_timestamp(unpacked):
-    
+    """Given an unpacked timestamp in list format, returns it in string format"""
     hour = str(unpacked[0])
     minute = str(unpacked[1])
     second = str(unpacked[2] / ts_sec_multiplier)
@@ -31,7 +31,7 @@ def format_unpacked_timestamp(unpacked):
 
 
 def unpack_timestamp(packed_ts):
-    
+    """Given a packed timestamp, returns its unpacked string"""
     unpacked_ts = struct.unpack(ts_format, packed_ts)
     unpacked_ts = format_unpacked_timestamp(unpacked_ts)
     
@@ -39,7 +39,7 @@ def unpack_timestamp(packed_ts):
 
 
 def pack_gnss(sensor_data):
-    
+    """Given sensor data, returns it in packed form"""
     packed_ts = pack_timestamp(sensor_data[0])
     lat = round(sensor_data[1] * gnss_multiplier)
     lon = round(sensor_data[2] * gnss_multiplier)
@@ -50,7 +50,7 @@ def pack_gnss(sensor_data):
 
 
 def unpack_gnss(packed_data):
-    
+    """Given packed sensor data, returns it unpacked in a tuple"""
     unpacked = struct.unpack(ts_format+gnss_format, packed_data)
     ts = format_unpacked_timestamp(unpacked)
     lat = unpacked[3] / gnss_multiplier
@@ -61,7 +61,7 @@ def unpack_gnss(packed_data):
 
 
 def pack_tmp117(sensor_data):
-    
+    """Given tmp117 data, returns it in packed form"""
     packed_ts = pack_timestamp(sensor_data[0])
     tmp = round(sensor_data[1] * tmp117_multiplier)
     packed = packed_ts + struct.pack(tmp117_format, tmp)
@@ -70,24 +70,25 @@ def pack_tmp117(sensor_data):
 
 
 def unpack_tmp117(packed_data):
-    
+    """Given packed tmp117 data, returns it in unpacked form"""
     unpacked = struct.unpack(ts_format+tmp117_format, packed_data)
     ts = format_unpacked_timestamp(unpacked)
     tmp = unpacked[3] / tmp117_multiplier
     
     return (ts, tmp)
 
-
+# TODO: Needs to be abstracted to loop through any given datasources
 def pack_all(sensor_data):
-    
+    """Given a dict of sensor data, returns it packed in a struct"""
     packed = \
         pack_gnss(sensor_data['gnss']) + \
         pack_tmp117(sensor_data['tmp117'])
     
     return packed
 
-
+# TODO: Needs to be abstracted to loop through any given datasources
 def get_combined_format():
+    """Returns the combined format to be used in Python struct"""
     combined_format = ts_format + \
                           gnss_format + \
                           tmp117_format \
@@ -96,7 +97,7 @@ def get_combined_format():
 
 
 def unpack_all(packed):
-    
+    """Given packed sensor data, returns its unpacked tuple"""
     unpacked = struct.unpack(get_combined_format(), packed)
     
     return unpacked
